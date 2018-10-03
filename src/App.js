85a80023f261e76c14f4a4383c5d45273ba88161
Jordan Schechter <jordanwschechter@gmail.com>
2018-10-03 15:57:05 -0600

    cards,json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component, Fragment } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./cards.json";
+import "./App.css";
 
 
 class App extends Component {
+
+  state = {
+    
+  }
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
