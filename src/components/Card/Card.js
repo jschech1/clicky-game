e64b0c250ff60a7b3b632e7adfa973d9517a9398
Jordan Schechter <jordanwschechter@gmail.com>
2018-10-03 17:03:56 -0600

    functioning code

---
+++ src/components/Card/Card.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const styles ={
+    card: {
+        display: "inline-block",
+        position: "relative",
+        background: "white",
+        textAlign: "left",
+        borderRadius: 5,
+        margin: "1%",
+        boxShadow: "0 3px 6px #999, 0 3px 6px #999"
+    },
+    img: {
+        // height: "100%",
+        overflow: "hidden",
+        textAlign: "center",
+        background: "white",
+        height: 100,
+        width: 220,
+        marginRight: "5%",
+        padding: 10
+    }
+}
+
+
+
+const Card = props => (
+    <div className="card" style={styles.card} onClick={() => props.clickCount(props.id)}>
+        <div className="img" >
+            <img alt={props.name} src={props.image} style={styles.img} />
+        </div>
+
+    </div>
+);
+
+export default Card;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = props => (
+    <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores"> Score: {props.score} Highscore: {props.highscore}</div>
+    </div>
+);
+
+export default Header;
