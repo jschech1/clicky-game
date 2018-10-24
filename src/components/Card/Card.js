9f695909b2b5a44fdae67834da34c4d1904626e7
Jordan Schechter <jordanwschechter@gmail.com>
2018-10-24 15:19:07 -0600

    text and module fix

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,15 +9,18 @@ const styles ={
         textAlign: "left",
         borderRadius: 5,
         margin: "1%",
-        boxShadow: "0 3px 6px #999, 0 3px 6px #999"
+        boxShadow: "0 3px 6px #999, 0 3px 6px #999",
+        width: "auto",
+        height: "auto",
+        marginLeft: "4%"
     },
     img: {
         // height: "100%",
         overflow: "hidden",
         textAlign: "center",
         background: "white",
-        height: 100,
-        width: 220,
+        height: 150,
+        width: 300,
         marginRight: "4%",
         padding: 10
     }
